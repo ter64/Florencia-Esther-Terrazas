e132bd2b17849587108861981a1043ab04d8c687
esther <estherrazas55@gmail.com>
2026-04-09 16:33:33 -0300

    POO EJERCICIOS

---
+++ src/POO/Perro.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POO;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Perro {
+    String raza;
+    String nombre;
+    
+    public Perro(String nombre, String raza) {
+        this.nombre = nombre;
+        this.raza = raza;
+    }
+    
+    void ladrar() {
+        System.out.println( nombre + ": Guau! Guau! ");
+    }
+
+}
---
+++ src/POO/mainEstudiante.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package POO;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class mainEstudiante {
+    public static void main(String[] args) {
+
+        Estudiante e1 = new Estudiante("Tiago", 17);
+        Estudiante e2 = new Estudiante("Isaias", 16);
+
+        e1.estudiar();
+        e2.rendir();
+        e1.comer();
+    }
+}
